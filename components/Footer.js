a65508e62035ad5a51c14fe844e1408cd520a330
Willy Wooder <87907037+willywooder@users.noreply.github.com>
2024-02-08 11:59:33 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
         <div className="p-5 footer">
             <img className="footer-logo" src="/img/madwithard_logo.png"></img>
             <div className="footer-links">
-                <a className="footer-link" href="/"><FaXTwitter /></a>
+                <a className="footer-link" href="https://twitter.com/WizardsMad"><FaXTwitter /></a>
             </div>
         </div>
     )
